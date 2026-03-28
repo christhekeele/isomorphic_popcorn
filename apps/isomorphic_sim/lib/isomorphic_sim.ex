70c8644947820bf86467c2f38570679a9a3fb3ae
Chris Keele <christhekeele@gmail.com>
2026-03-28 17:14:18 -0500

    Run `cd apps; mix new isomorphic_sim --sup`

---
+++ apps/isomorphic_sim/lib/isomorphic_sim.ex
@@ -0,0 +1,18 @@
+defmodule IsomorphicSim do
+  @moduledoc """
+  Documentation for `IsomorphicSim`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> IsomorphicSim.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/isomorphic_sim/test/isomorphic_sim_test.exs
@@ -0,0 +1,8 @@
+defmodule IsomorphicSimTest do
+  use ExUnit.Case
+  doctest IsomorphicSim
+
+  test "greets the world" do
+    assert IsomorphicSim.hello() == :world
+  end
+end
